9366aebaa82034cc37775b68c6264e685f5d2ae1
Hossein-AliHosseini <themmdhossein@gmail.com>
2023-03-09 23:56:43 +0330

    Fix bug in thread.h

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -113,9 +113,9 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
-    struct list fd_list;
+    // struct list fd_list;
 
-    int fd_count; 
+    // int fd_count; 
    
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
