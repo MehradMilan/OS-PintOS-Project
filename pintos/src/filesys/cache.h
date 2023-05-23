c671cf1f35548f7992896231acf661b3b87512d5
ALIREZA NOROOZI <noroozi.alirezaa@gmail.com>
2023-05-23 14:13:42 +0330

    check for test phase 1

--- pintos/src/filesys/cache.h
+++ pintos/src/filesys/cache.h
@@ -5,61 +5,36 @@
 #ifndef FILESYS_CACHE_H
 #define FILESYS_CACHE_H
 
-#include "devices/block.h"
-#include "lib/kernel/list.h"
 #include "off_t.h"
+#include "devices/block.h"
 #include "threads/synch.h"
+#include "lib/kernel/list.h"
 
-#define BUFFER_CACHE_SIZE 64
+
+#define CACHE_SIZE 64
+
 
 struct cache_block
-  {
-    block_sector_t sector;
+{
+    block_sector_t sector_num;
+    bool valid;
+    bool dirty;
     char data[BLOCK_SECTOR_SIZE];
-    int dirty;
-    int valid;
-    struct lock c_lock;
+    struct list_elem cache_elem;
+    struct lock cache_lock;
+};
 
-  #ifdef SHAKH
-    struct condition ok_to_read;
-    struct condition ok_to_write;
-    int waiting_w;
-    int waiting_r;
-    int available_w;
-    int available_r;
-  #endif
 
-    struct list_elem elem;
-  };
+struct cache_block cache[CACHE_SIZE];
+struct list cache_LRU;
+struct lock cache_list_lock;
 
-struct cache_block cache_blocks[BUFFER_CACHE_SIZE];
-struct list LRU;
-struct lock LRU_modify_lock;
-int LRU_hit;
-int LRU_miss;
-int LRU_write;
 
-/* Initialize cache. */
 void cache_init (void);
-
-/* Read chunk_size bytes of data from cache starting from sector_index at position offest,
-   into destination. */
-void cache_read (struct block *, block_sector_t, void *, off_t, int);
-
-/* Write chunk_size bytes of data into cache starting from sector_index at position offest,
-   from source. */
-void cache_write (struct block *, block_sector_t, void *, off_t, int);
-
-/* Write-back all cache blocks and invalidate them.*/
-void cache_flush (struct block *);
-
-/* cache stats API for SYS_CACHE_SPEC syscall */
-uint32_t cache_get_total_read_cnt (void);
-uint32_t cache_get_total_write_cnt (void);
-uint32_t cache_get_total_miss (void);
-uint32_t cache_get_total_hit (void);
-
-/* invalidates all cache blocks */
-void cache_invalidate (struct block *fs_device);
+void cache_read (struct block *fs_device, block_sector_t sector_idx, void *buffer, off_t offset, int chunk_size);
+void cache_write (struct block *fs_device, block_sector_t sector_idx, void *buffer, off_t offset, int chunk_size);
+void flush_block (struct block *fs_device, struct cache_block *LRU_block);
+int get_cache_index(block_sector_t sector);
+struct cache_block *get_cache_block (struct block *fs_device, block_sector_t sector);
 
 #endif /* filesys/cache.h */
